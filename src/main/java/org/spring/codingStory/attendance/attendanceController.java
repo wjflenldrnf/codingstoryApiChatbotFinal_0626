2ef4311504cbf410c3691f43a7abd0cbde0db3f4
thisisticket <leedlrjsvy1@gmail.com>
2024-05-14 09:54:40 +0900

    0514

--- src/main/java/org/spring/codingStory/attendance/attendanceController.java
+++ src/main/java/org/spring/codingStory/attendance/attendanceController.java
@@ -6,16 +6,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class attendanceController {
 
-  @GetMapping({"","/index","/a"})
-  public String index(){
-    return "a";
-  }
-  @GetMapping("/b")
-  public String bindex(){
-    return "b";
-  }
-  @GetMapping("/admin")
-  public String admin(){
-    return "admin";
-  }
+
 }
