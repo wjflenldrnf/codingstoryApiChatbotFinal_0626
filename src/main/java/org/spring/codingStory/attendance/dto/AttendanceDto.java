2a2cde439d6396d6f177e3745a1b55d5fe0f901f
wjflenldrnf <wjflenldrnf@gmail.com>
2024-05-14 11:32:09 +0900

    att

--- src/main/java/org/spring/codingStory/attendance/dto/AttendanceDto.java
+++ src/main/java/org/spring/codingStory/attendance/dto/AttendanceDto.java
@@ -1,14 +1,9 @@
 package org.spring.codingStory.attendance.dto;
 
 import lombok.*;
-import org.spring.codingStory.attendance.AttendanceStatus;
-import org.spring.codingStory.attendance.entity.AttendanceEntity;
 import org.spring.codingStory.member.entity.MemberEntity;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,37 +12,15 @@ import java.util.List;
 @Setter
 public class AttendanceDto {
 
+    private Long id;
 
-    @Builder
-    @Getter @Setter
-    public static class Index {
-        private LocalDate attDate;
-        private LocalTime attOnTime;
-        private LocalTime attOffTime;
-        private AttendanceStatus attStatus;
-        private String empName;
-        private String empPosition;
-        private String deptName;
-        private String dayOfWeek;
-    }
+    private MemberEntity memberEntity;
 
-    @Builder
-    @Getter @Setter
-    public static class Status {
-        private int onCnt;
-        private int offCnt;
-        private int absenceCnt;
-        private int lateCnt;
-        private int vacationCnt;
-        private int sickCnt;
-    }
+    private LocalDateTime checkInTime;
 
-    @Getter @Setter
-    public static class StatusAndIndexWithPage {
-        private AttendanceDto.Status status;
-        private List<Index> attendanceList;
-//        private PageResultDTO pageResultDTO;
-    }
+    private LocalDateTime checkOutTime;
+
+    private String attendanceType;
 
 }
 
